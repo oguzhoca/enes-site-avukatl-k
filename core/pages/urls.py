914e7e38366c75de2909845013978652389ea88b
oguzhoca <oguzaydn33@gmail.com>
2021-09-07 12:19:08 +0300

    sitenin çoğu işi bitti döngü yazılmadı

---
+++ core/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import AboutView, IndexView
+from . import views
+
+
+
+urlpatterns = [
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
+    #path('contact/', ContactView.as_view(), name="contact"),
+]
